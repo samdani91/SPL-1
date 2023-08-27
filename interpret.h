1384e5fa1586107acbcfa003677fbc865344f964
A. M Samdani Mozumder <114305338+samdani91@users.noreply.github.com>
2023-08-28 00:01:00 +0600

    fourth commit

--- interpret.h
+++ interpret.h
@@ -10,6 +10,8 @@ void read_main_prototype(string line, string tokenValue);
 void check_open_bracket(int lineNum, string tokenValue);
 void read_var(unordered_map<string, string> map, int currlineNum);
 void read_condition(unordered_map<string, string> condition, int lineNum);
+void read_if_else_block(unordered_multimap<string, string> statements, int lineNum);
+void read_printf(unordered_multimap<string, string> print, int lineNum);
 
 vector<string> header = {"stdio.h", "conio.h", "stdlib.h", "math.h", "string.h"};
 map<string, string> operators = {{"!=", "not equal "},
@@ -18,6 +20,10 @@ map<string, string> operators = {{"!=", "not equal "},
                                  {"<=", "less than or equal "},
                                  {">", "greater than "},
                                  {"<", "less than "}};
+map<string, string> fm_spec = {{"%d", "integer "},
+                                {"%dn", "integer in every new line "},
+                               {"%f", "float "},
+                               {"%fn", "float in every new line "}};
 unordered_map<string, float> traceVar;
 
 #endif
