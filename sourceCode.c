3fc4d324f19aa79c78f373306ede707a01f616f4
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-07 01:16:20 +0600

    third commit

--- sourceCode.c
+++ sourceCode.c
@@ -6,12 +6,16 @@
 int main()
 {
     float x=11;
+    float y=15;
+
+    float sum=x+y;
     for(int i=1;i<=10;i++){
         printf("%d\n",i);
     }
     int j=0;
     while(j<5){
         printf("%d",j);
+        j++;
     }
     if(x!=10){
         printf("%f",x);
