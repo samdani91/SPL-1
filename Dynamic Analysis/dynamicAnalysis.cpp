d92470b38ad97ed7f2d218e6cf479c8bb8bf5ef9
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-30 01:57:46 +0600

    third commit

--- Dynamic Analysis/dynamicAnalysis.cpp
+++ Dynamic Analysis/dynamicAnalysis.cpp
@@ -1,36 +1,42 @@
 #include<bits/stdc++.h>
-#include "/home/samdani1412/Desktop/SPL-1/interpret.h"
-#include "/home/samdani1412/Desktop/SPL-1/interpretFunc.cpp"
+#include "/home/samdani1412/Desktop/temp/SPL/interpret.h"
+#include "/home/samdani1412/Desktop/temp/SPL/interpretFunc.cpp"
 using namespace std;
 
 
 int findLineNum(ifstream& inputFile);
-void  moveFilePointer(ifstream& inputFile,int readLinenum);
+void  moveFilePointer(fstream& inputFile,int readLinenum);
 
 int main()
 {
 
-    ifstream file("/home/samdani1412/Desktop/SPL-1/sourceCode.c");
+    ifstream file("/home/samdani1412/Desktop/temp/SPL/sourceCode.c");
 
     if(!(file.is_open())){
         cout<<"Error in opening Source Code file"<<endl;
         return 0;
     }
 
-    ifstream file2("/home/samdani1412/Desktop/SPL-1/Dynamic Analysis/testCode.c");
+    ifstream file2("da.c");
 
     int writeLineNum=findLineNum(file2);
+
+    file2.close();
+
     int readLineNum=findLineNum(file);
     file.close();
 
-    ifstream file3("/home/samdani1412/Desktop/SPL-1/sourceCode.c");
+    fstream file3("/home/samdani1412/Desktop/temp/SPL/sourceCode.c");
     moveFilePointer(file3,readLineNum);
+
+    fstream file4("da.c");
+    moveFilePointer(file4,writeLineNum);
 
     string line;
 
-    // while(getline(file3,line)){
-    //     cout<<line<<endl;
-    // }
+    while(getline(file3,line)){
+        file4<<line<<endl;
+    }
     
 
     
@@ -79,13 +85,13 @@ int findLineNum(ifstream& inputFile)
     }
     return targetLineNum;
 }
-void  moveFilePointer(ifstream& inputFile,int readLinenum)
+void  moveFilePointer(fstream& inputFile,int readLinenum)
 {
     string line;
-    int currLineNum=0;
+    int currLineNum=1;
     while(getline(inputFile,line)){
-        cout<<line<<endl;
-        if(currLineNum==readLinenum){
+        //cout<<line<<endl;
+        if(currLineNum==readLinenum-1){
             break;
         }
         currLineNum++;
