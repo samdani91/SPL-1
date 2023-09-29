6d766c9f5fac3a47bf0ec6aa16a53ec1eb1c4209
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-30 00:41:09 +0600

    second commit

--- Dynamic Analysis/dynamicAnalysis.cpp
+++ Dynamic Analysis/dynamicAnalysis.cpp
@@ -1,24 +1,59 @@
 #include<bits/stdc++.h>
+#include "/home/samdani1412/Desktop/SPL-1/interpret.h"
+#include "/home/samdani1412/Desktop/SPL-1/interpretFunc.cpp"
 using namespace std;
+
+
+int findLineNum(ifstream& inputFile);
+void  moveFilePointer(ifstream& inputFile,int readLinenum);
 
 int main()
 {
 
+    ifstream file("/home/samdani1412/Desktop/SPL-1/sourceCode.c");
 
-    ifstream file2("testCode.c");
-
-    if(!(file2.is_open())){
-        cout<<"Error in opening file"<<endl;
+    if(!(file.is_open())){
+        cout<<"Error in opening Source Code file"<<endl;
         return 0;
+    }
+
+    ifstream file2("/home/samdani1412/Desktop/SPL-1/Dynamic Analysis/testCode.c");
+
+    int writeLineNum=findLineNum(file2);
+    int readLineNum=findLineNum(file);
+    file.close();
+
+    ifstream file3("/home/samdani1412/Desktop/SPL-1/sourceCode.c");
+    moveFilePointer(file3,readLineNum);
+
+    string line;
+
+    // while(getline(file3,line)){
+    //     cout<<line<<endl;
+    // }
+    
+
+    
+
+    return 0;
+}
+int findLineNum(ifstream& inputFile)
+{
+    
+
+    if(!(inputFile.is_open())){
+        cout<<"Error in opening file"<<endl;
+        return -1;
     }
 
     string line;
     bool isMainWithout=false;
     bool isMainWith=false;
     int lineNum=0,targetLineNum;
-    while(!file2.eof()){
 
-        getline(file2,line);
+    while(getline(inputFile,line)){
+
+        
         lineNum++; 
 
         if(line.find("main")!=string::npos && line.find("{")==string::npos){
@@ -29,7 +64,7 @@ int main()
 
         if(isMainWithout){
             lineNum++;
-            getline(file2,line);
+            getline(inputFile,line);
             lineNum++;
             if(line.find("{")!=string::npos){
                 targetLineNum=lineNum;
@@ -41,9 +76,19 @@ int main()
             isMainWith=false;
         }
 
-        // lineNum++;
     }
-    cout<<targetLineNum<<endl;
-
-    return 0;
+    return targetLineNum;
+}
+void  moveFilePointer(ifstream& inputFile,int readLinenum)
+{
+    string line;
+    int currLineNum=0;
+    while(getline(inputFile,line)){
+        cout<<line<<endl;
+        if(currLineNum==readLinenum){
+            break;
+        }
+        currLineNum++;
+    }
+    //return inputFile;
 }
