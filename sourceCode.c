145b08f3ecf3d7688dc8a7dcf662c2c2ee6e016c
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-11-06 21:05:22 +0600

    pointer variable added

--- sourceCode.c
+++ sourceCode.c
@@ -5,26 +5,12 @@
 
 int main()
 {
-    float x=11;
-    printf("%f",x);
-    float y=15;
-    int z;
-    scanf("%d",&z);
+    int x,y;
+    x=12;
+    y=15;
 
-    for(int i=1;i<=3;i++){
-        printf("%d",i);
-    }
-    int j=0;
-    while(j<3){
-        printf("%d",j);
-        j++;
-    }
-    if(x!=10){
-        printf("HelloWorld");
-    }
-    else{
-        printf("TataWorld");
-    }
+    x=20;
+    y=30;
 
     return 0;
 }
