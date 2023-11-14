ae2cc13879ab6e30536c948ce314cbba0572f3a1
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-11-14 13:26:26 +0600

    ampersand check added

--- errorChecking.cpp
+++ errorChecking.cpp
@@ -20,3 +20,21 @@ void check_semi_colon(unordered_multimap<string, string> statement, int lineNum)
         cout << "\033[0m";
     }
 }
+
+void check_Ampersand(unordered_multimap<string,string>statment,int lineNum)
+{
+    bool isAmpersand=false;
+    auto range=statment.equal_range("operator");
+
+    for(auto it=range.first;it!=range.second;it++){
+        string temp=it->second;
+        if(temp==",&"){
+            isAmpersand=true;
+        }
+    }
+
+    if(!isAmpersand){
+        cout <<"\033[35m\t\twarning: expected '&' [-Wformat=]"<<endl;
+        cout << "\033[0m";
+    }
+}
