e5972d2826d6b2db72f269c318ae1dea047d5ec0
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-12-16 23:45:33 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@
 
 int main()
 {
-    string inputCode = readInputCode(); // Read the input code from "sourceCode.c"
-    int totalLines = inputCodeInLineByLine(inputCode); // Tokenize the input code and get the total number of lines
-
-    tokenization(totalLines); // Perform tokenization and write the tokens to "TokenFile.txt"
-
+    string inputCode = readInputCode();
+    int totalLines = inputCodeInLineByLine(inputCode);
+    cout<<totalLines<<endl;
+    tokenization(totalLines);
     cout<<"\t\t\t---------------Interpretation----------------"<<endl;
     cout<<endl;
 
